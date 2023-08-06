5085fc23d29b875dd5b30ff2a0d5c67b1133af44
Alan Cruz <alansasuke0@gmail.com>
2023-08-05 23:45:40 -0600

    relacion de modelos

--- app/Models/Dispositivo.php
+++ app/Models/Dispositivo.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Dispositivo extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    public function user()
+    {
+        return $this->belongsToMany(User::class, 'users_dispositivos', 'dispositivo_id', 'user_id');
+    }
+    
+    public function sensor()
+    {
+        return $this->hasMany(Sensor::class, 'dispositivo_id');
+    }
 }
--- app/Models/Sensor.php
+++ app/Models/Sensor.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Sensor extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    public function dispositivo()
+    {
+        return $this->belongsTo(Dispositivo::class, 'dispositivo_id');
+    }
+    
+    public function registro()
+    {
+        return $this->hasMany(Registro::class, 'sensor_id');
+    }
 }
