a582cb33f941ce8dc78c3ddce370cfdffa0ffca0
Alan Cruz <alansasuke0@gmail.com>
2023-08-06 03:55:37 -0600

    seeder de dispositivos

--- database/seeders/DispositivoSeeder.php
+++ database/seeders/DispositivoSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DispositivoSeeder extends Seeder
 {
@@ -14,6 +15,19 @@ class DispositivoSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $data = [
+            ['alias' => 'Bar El Rincon', 'descripcion' => 'Calle Juarez 123, Torreon, Coahuila', 'codigo' => rand(1000, 9999)],
+            ['alias' => 'La Cantina del Pueblo', 'descripcion' => 'Avenida Hidalgo 456, Torreon, Coahuila', 'codigo' => rand(1000, 9999)],
+            ['alias' => 'El Meson de los Amigos', 'descripcion' => 'Calle Zaragoza 789, Torreon, Coahuila', 'codigo' => rand(1000, 9999)],
+            ['alias' => 'Cerveceria La Terraza', 'descripcion' => 'Boulevard Revolucion 234, Torreon, Coahuila', 'codigo' => rand(1000, 9999)],
+            ['alias' => 'Bar El Patio', 'descripcion' => 'Avenida Morelos 567, Torreon, Coahuila', 'codigo' => rand(1000, 9999)],
+            ['alias' => 'Cantina La Tradicion', 'descripcion' => 'Calle Madero 890, Torreon, Coahuila', 'codigo' => rand(1000, 9999)],
+            ['alias' => 'Taberna Los Charros', 'descripcion' => 'Boulevard Constitucion 901, Torreon, Coahuila', 'codigo' => rand(1000, 9999)],
+            ['alias' => 'Bar La Esquina', 'descripcion' => 'Avenida Allende 345, Torreon, Coahuila', 'codigo' => rand(1000, 9999)],
+            ['alias' => 'Cerveceria El Alamo', 'descripcion' => 'Calle Obregon 678, Torreon, Coahuila', 'codigo' => rand(1000, 9999)],
+            ['alias' => 'Cantina El Farol', 'descripcion' => 'Boulevard Miguel Hidalgo 1234, Torreon, Coahuila', 'codigo' => rand(1000, 9999)],
+        ];
+
+        DB::table('dispositivos')->insert($data);
     }
 }
