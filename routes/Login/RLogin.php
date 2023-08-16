9cdeef78f12a43cf2b4a124ffd804c4e1d4c7905
IssacGarcia <issacgar98@gmail.com>
2023-08-15 18:39:39 -0600

    cambio dentro de las rutas

--- routes/Login/RLogin.php
+++ routes/Login/RLogin.php
@@ -3,10 +3,13 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
 
-Route::get('/check', function() { return 'ok'; });
-Route::post('/login', [LoginController::class, 'login']);
-Route::post('/register', [LoginController::class, 'register']);
-Route::post('/logout', [LoginController::class, 'logout'])->middleware('auth:sanctum');
-Route::get('/validate', [LoginController::class, 'validar']);
+Route::prefix('user')->group(function () {
+    Route::get('/check', function() { return 'ok'; });
+    Route::post('/login', [LoginController::class, 'login']);
+    Route::post('/register', [LoginController::class, 'register']);
+    Route::post('/logout', [LoginController::class, 'logout'])->middleware('auth:sanctum');
+    Route::get('/validate', [LoginController::class, 'validar']);
+});
+
 
 Route::get('/user', [LoginController::class,'getUserData']);
